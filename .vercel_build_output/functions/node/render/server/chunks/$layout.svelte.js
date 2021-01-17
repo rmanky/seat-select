4a6d0f12922a8fa574d5c10643da74ccd98d10d3
Robear Mankaryous <rmankaryous12@gmail.com>
2021-01-17 17:31:20 -0500

    netlify?

--- .vercel_build_output/functions/node/render/server/chunks/$layout.svelte.js
+++
@@ -1,2 +0,0 @@
-"use strict";require("@sveltejs/kit/dist/renderer");const e=require("./app.js").create_ssr_component(((e,t,r,s)=>`<main>${s.default?s.default({}):""}</main>`));exports.default=e;
-//# sourceMappingURL=$layout.svelte.js.map
--- .vercel_build_output/static/_app/entry-dfaaaadb.js
+++
@@ -1,2 +0,0 @@
-export{p as start}from"./start-651b6ff1.js";
-//# sourceMappingURL=entry-dfaaaadb.js.map
