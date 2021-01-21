de5c72d6f27c10b5c3c77ebb7b9bddcc7fec5360
Robear Mankaryous <rmankaryous12@gmail.com>
2021-01-21 18:29:08 -0500

    time to throw

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -15,7 +15,9 @@
 
     function updateContacts(event) {
         contacts = event.detail;
-        selected = selected.filter(id => contacts.filter(i => i.id == id).length != 0);
+        selected = selected.filter(
+            (id) => contacts.filter((i) => i.id == id).length != 0
+        );
     }
 
     export function addContact(name, number) {
@@ -35,13 +37,14 @@
 
     export async function sendTo() {
         sendEnabled = false;
-        const target = contacts.filter((contact) => selected.includes(contact.id));
+        const target = contacts.filter((contact) =>
+            selected.includes(contact.id)
+        );
         const payload = {};
         payload["contacts"] = target;
 
-        const rawResponse = await fetch(
-            "https://cors-anywhere.herokuapp.com/http://send-sms-5601.twil.io/send-sms",
-            {
+        try {
+            await fetch("https://send-sms-5601.twil.io/send-sms", {
                 method: "POST",
                 mode: "cors",
                 headers: {
@@ -49,14 +52,12 @@
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify(payload),
-            }
-        );
-
-        const body = await rawResponse.text();
-        if (body == "success") {
-            firestore.sentText(target);
+            });
+        } catch (err) {
+            console.log(err);
         }
-        sendEnabled = true;
+
+        selected = [];
     }
 </script>
 
@@ -65,51 +66,59 @@
     <div class="container">
         {#if contacts}
             <table
-                class="w-full flex flex-row flex-no-wrap sm:bg-gray-800 overflow-hidden sm:rounded-lg sm:shadow-lg my-5"
+                class="w-full flex flex-row flex-no-wrap md:bg-gray-800 overflow-hidden md:rounded-lg md:shadow-lg my-5"
             >
                 <thead class="text-white">
                     {#each contacts as _}
                         <tr
-                            class="flex flex-col bg-gray-700 flex-no wrap sm:table-row rounded-l-xl sm:rounded-none mb-2 sm:mb-0">
+                            class="flex flex-col bg-gray-700 flex-no wrap md:table-row rounded-l-xl md:rounded-none mb-2 md:mb-0">
                             <th class="p-3 text-left">Select</th>
-                            <th class="p-3 text-left border-gray-500">Name</th>
+                            <th class="p-3 text-left">Name</th>
                             <th class="p-3 text-left">Number</th>
                             <th class="p-3 text-left">ID</th>
                             <th class="p-3 text-left">Status</th>
-                            <th class="p-3 text-left" width="110px">Remove</th>
+                            <th class="p-3 text-left" width="110px">Delete</th>
                         </tr>
                     {/each}
                 </thead>
-                <tbody class="flex-1 sm:flex-none">
+                <tbody class="flex-1 md:flex-none">
                     {#each contacts as { name, number, id, status }}
                         <tr
-                            class={`flex flex-col ${selected.includes(id) ? 'bg-blue-800' : 'bg-gray-800'} sm:border-b-2 border-gray-600 rounded-r-xl flex-no wrap sm:table-row mb-2 sm:mb-0`}>
+                            class={`flex flex-col ${
+                                selected.includes(id)
+                                    ? "bg-blue-800"
+                                    : "bg-gray-800"
+                            } md:border-b-2 border-gray-600 rounded-r-xl flex-no wrap md:table-row mb-2 md:mb-0`}>
                             {#if selected.includes(id)}
                                 <button
-                                    class="m-0 p-3 rounded-none rounded-tr-xl sm:m-3 sm:rounded bg-yellow-600 text-left hover:font-medium cursor-pointer"
-                                    on:click={() => selected = selected.filter(i => i != id)}><i class="fas fa-minus"></i></button>
+                                    class="m-0 p-3 rounded-none rounded-tr-xl md:m-3 md:rounded bg-yellow-600 text-left hover:font-medium cursor-pointer"
+                                    on:click={() =>
+                                        (selected = selected.filter(
+                                            (i) => i != id
+                                        ))}><i class="fas fa-minus" /></button
+                                >
                             {:else}
                                 <button
-                                    class="m-0 p-3 rounded-none rounded-tr-xl sm:m-3 sm:rounded bg-green-600 text-left hover:font-medium cursor-pointer"
+                                    class="m-0 p-3 rounded-none rounded-tr-xl md:m-3 md:rounded bg-green-600 text-left hover:font-medium cursor-pointer"
                                     on:click={() =>
                                         (selected = [...selected, id])}>
-                                    <i class="fas fa-plus"></i>
+                                    <i class="fas fa-plus" />
                                 </button>
                             {/if}
                             <td
-                                class="p-3 text-left sm:border-r-2 border-gray-600"
+                                class="p-3 text-left md:border-r-2 border-gray-600"
                                 >{name}</td
                             >
                             <td
-                                class="p-3 text-left sm:border-r-2 border-gray-600"
+                                class="p-3 text-left md:border-r-2 border-gray-600"
                                 >{number}</td
                             >
                             <td
-                                class="p-3 text-left sm:border-r-2 border-gray-600"
+                                class="p-3 text-left md:border-r-2 border-gray-600"
                                 >{id}</td
                             >
                             <td
-                                class="p-3 text-left sm:border-r-2 border-gray-600"
+                                class="p-3 text-left md:border-r-2 border-gray-600"
                                 ><i
                                     class={`fas fa-circle mr-2 ${
                                         status > 0
@@ -121,9 +130,9 @@
                                     : "Not Sent"}</td
                             >
                             <button
-                                class="m-0 p-3 rounded-none rounded-br-xl sm:m-3 sm:rounded bg-red-600 text-left hover:font-medium cursor-pointer"
+                                class="m-0 p-3 rounded-none rounded-br-xl md:m-3 md:rounded bg-red-600 text-left hover:font-medium cursor-pointer"
                                 on:click={() => firestore.deletePerson(id)}>
-                                Remove
+                                <i class="fas fa-minus" />
                             </button>
                         </tr>
                     {/each}
@@ -134,7 +143,7 @@
 </body>
 
 <style>
-    @media (min-width: 640px) {
+    @media (min-width: 768px) {
         table {
             display: inline-table !important;
         }
